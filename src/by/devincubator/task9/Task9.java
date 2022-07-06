7bc8f103245410623cfeadd9e1ffe3826bb6a794
Anton Novosad <noobito.2011@gmail.com>
2022-07-06 19:44:09 +0300

    changed main;

--- src/by/devincubator/task9/Task9.java
+++ src/by/devincubator/task9/Task9.java
@@ -7,6 +7,31 @@ import by.devincubator.task9.animals.Kangaroo;
 
 public class Task9 {
     public static void main(String[] args) {
+        Animal[] herd = createAnimalsArray();
+
+        Horse horse = new Horse("Horse 3", 4, false);
+        Kangaroo kangaroo = new Kangaroo("Kangaroo 3", 4, true);
+        Dragonfly dragonfly = new Dragonfly("Dragonfly 3", 6, true);
+
+        voiceAnimals(herd);
+        horse.voice();
+        kangaroo.voice();
+        dragonfly.voice();
+
+        System.out.println();
+        moveAnimals(herd);
+        horse.move();
+        kangaroo.move();
+        dragonfly.move();
+
+        System.out.println();
+        isHungryAnimals(herd);
+        horse.isHungry();
+        kangaroo.isHungry();
+        dragonfly.isHungry();
+    }
+
+    private static Animal[] createAnimalsArray() {
         Animal[] herd = new Animal[7];
         herd[0] = new Horse("Horse", 4, false);
         herd[1] = new Horse("Horse 2", 4, false);
@@ -15,32 +40,24 @@ public class Task9 {
         herd[4] = new Dragonfly("Dragonfly", 6, true);
         herd[5] = new Dragonfly("Dragonfly 2", 6, true);
         herd[6] = new Animal("Unknown type", 10, true) {};
+        return herd;
+    }
 
-        Horse horse = new Horse("Horse 3", 4, false);
-        Kangaroo kangaroo = new Kangaroo("Kangaroo 3", 4, true);
-        Dragonfly dragonfly = new Dragonfly("Dragonfly 3", 6, true);
-
+    private static void voiceAnimals(Animal[] herd) {
         for (Animal a : herd) {
             a.voice();
         }
-        horse.voice();
-        kangaroo.voice();
-        dragonfly.voice();
+    }
 
-        System.out.println();
+    private static void moveAnimals(Animal[] herd) {
         for (Animal a : herd) {
             a.move();
         }
-        horse.move();
-        kangaroo.move();
-        dragonfly.move();
+    }
 
-        System.out.println();
+    private static void isHungryAnimals(Animal[] herd) {
         for (Animal a : herd) {
             a.isHungry();
         }
-        horse.isHungry();
-        kangaroo.isHungry();
-        dragonfly.isHungry();
     }
 }
